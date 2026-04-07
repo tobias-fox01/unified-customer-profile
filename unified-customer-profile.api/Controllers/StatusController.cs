35c795229ef52d8df4d81b9c0f2aced77ee7cd8f
Toby Fox <toby-fox2001@outlook.com>
2026-04-07 19:22:05 +0100

    feature: Added pull_request workflow for project
    Added pull_request workflow for project
    Added controller for status endpoint

---
+++ unified-customer-profile.api/Controllers/StatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace unified_customer_profile.api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
