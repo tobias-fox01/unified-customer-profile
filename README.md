aea5ccf48e3048e28273e03886de21f9223dbf0a
Toby Fox <toby-fox2001@outlook.com>
2026-05-07 17:25:10 +0200

    feat(003): Added setup project to create a cosmos db for the CMS
    Added a setup project that will take a json file and upload it as a database to the cosmosdb emulator. The values for the client, database, and containers are stored on a appsettings file for the setup project. Also, add a compose.yml, so the emulator can be setup with podman for any user of the project.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# unified-customer-profile
-A middleware platform for aggregating and normalising customer data across services
+## unified-customer-profile
+
+## About The Project
+
+Unified Customer Profile
+A middleware platform for aggregating and normalising customer data across services.
+The aim of this project is create endpoints that aggregating across multiple data sources that would reflect a real life system or company that will have data fragmented across multiple interal and external rescources.
+
+### Built With
+
+- .Net v10.0.0
+
+## Setup
+
+1. Clone the repository
+```sh
+git clone git@github.com:tobias-fox01/unified-customer-profile.git
+```
+
+2. Install dependancies
+```sh
+dotnet restore
+```
+
+3. Build Project
+```sh
+dotnet build
+```
+
+To setup this project, you must locally create the cosmos databases which will represent mock external systems.
+4. Create the container using Podman to run cosmos db emulator
+```sh
+podman compose --file compose.yml up --detach
+```
+
+5. To upload the data from json to cosmos db database run
+```sh
+dotnet run --project ./unified-customer-profile.Setup
+```
---
+++ unified-customer-profile.Setup/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Cosmos;
+using unified_customer_profile.Setup;
+using unified_customer_profile.Setup.Models;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.LocalHost.json")
+    .Build();
+
+try
+{
+    // Initalise Cosmos Client
+    CosmosClientOptions options = new()
+    {
+        ConnectionMode = ConnectionMode.Gateway,
+        LimitToEndpoint = true,
+        HttpClientFactory = () => new HttpClient(new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        })
+    };
+
+    string? cosmosHost = config["CosmosDB:Host"];
+    string? cosmosAccountKey = config["CosmosDB:Host"];
+
+    if (cosmosHost is null)
+    {
+        throw new Exception("CosmosDB Host is not set.");
+    }
+    if (cosmosAccountKey is null)
+    {
+        throw new Exception("CosmosDB Account Key is not set.");
+    }
+    CosmosClient client = new(config["CosmosDB:Host"], config["CosmosDB:AccountKey"], options);
+
+    // Initalise Container for CMS Customers
+    var customerCMSConfig = new ContainerConfig
+    {
+        DataFile = config["CMS:DataDir"],
+        DatabaseId = config["CMS:DatabaseName"],
+        ContainerId = config["CMS:Customers:ContainerName"],
+        PartitionKey = config["CMS:Customers:PartitionKey"],
+    };
+    Console.WriteLine("Creating CMS Customers Container");
+    await SetupCosmos.InitaliseContainer(client, customerCMSConfig);
+    Console.WriteLine("Completed CMS Customers Container");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
